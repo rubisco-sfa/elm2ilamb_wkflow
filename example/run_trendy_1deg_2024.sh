06297e331d900b71e2cc140e7b3ad0e9f00f8a5d
Min Xu <min.mxu@gmail.com>
2024-09-06 14:05:52 -0400

    add the cmorization for e3sm-trendy simulations

--- example/run_trendy_1deg_2024.sh
+++ example/run_trendy_1deg_2024.sh
@@ -1,34 +1,29 @@
-#!/usr/bin/bash
-
+#!/usr/bin/env bash
 
 
 export HDF5_USE_FILE_LOCKING=FALSE
 
-#-CaseNames=(\
-#-           20230926_S0_f09_f09_ICB20TRCNPRDCTCBC \
-#-           20230926_S1_f09_f09_ICB20TRCNPRDCTCBC \
-#-           20230926_S3_f09_f09_ICB20TRCNPRDCTCBC)
-
 CaseNames=(\
-           20230926_S2_f09_f09_ICB20TRCNPRDCTCBC)
+           20240903_TRENDY_f09_S0)
 
            #20230830_S1_f09_f09_ICB20TRCNPRDCTCBC \
            #20230830_S2_f09_f09_ICB20TRCNPRDCTCBC \
            #20230830_S3_f09_f09_ICB20TRCNPRDCTCBC \
            #20230830_S0_f09_f09_ICB20TRCNPRDCTCBC)
 
-OutDir=/compyfs/ricc364/e3sm_scratch/
+OutDir=/gpfs/wolf2/cades/cli185/scratch/xyk/e3sm_run/
+CmrDir=/gpfs/wolf2/cades/cli185/scratch/mfx/foryxj/trendy_postprocess/fv09
 
+Script=`readlink -f $0`
+SrcDir=`dirname $Script`
 
-CmrDir=/qfs/people/xumi699/compyfs/scratch/forxj/trendy_postprocess/new_fv09
 
 for cname in "${CaseNames[@]}"; do
     echo x${cname}x
     echo $OutDir/$cname/run/
-    ./elm_singlevar_ts.bash -c $cname -y 1700-2022 -a 0 -i $OutDir/$cname/run/ -o $CmrDir \
-               -e TRENDY -m e3sm --ncclimo --prepcmor --linkfil
+    $SrcDir/../elm_singlevar_ts.bash -c $cname -y 1700-2023 -a 0 -i $OutDir/$cname/run/ -o $CmrDir \
+               -e TRENDY2024 -m e3sm --ncclimo --prepcmor 
 done
-
 
 # get fix data
 #./elm_singlevar_ts.bash -c $cname -y 1700-2022 -a 0 -i $OutDir/$cname/run/ -o $CmrDir \
