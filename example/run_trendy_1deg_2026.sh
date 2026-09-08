d2cbc1fab2f1a5d7162772aca67469f2873d3a89
Min Xu <min.mxu@gmail.com>
2026-09-08 08:41:50 -0400

    add missing vars to the trendy 2026 list

--- example/run_trendy_1deg_2026.sh
+++ example/run_trendy_1deg_2026.sh
@@ -4,15 +4,13 @@
 export HDF5_USE_FILE_LOCKING=FALSE
 
 CaseNames=(\
-           20260820_TRENDY2026_f09_S2 )
-
-#-           20260820_TRENDY2026_f09_S0 \
-#-           20260820_TRENDY2026_f09_S1 \
-#-           20260820_TRENDY2026_f09_S2 \
-#-           20260820_TRENDY2026_f09_S3 \
-#-           20260820_TRENDY2026_f09_S4 \
-#-           20260820_TRENDY2026_f09_S5 \
-#-           20260820_TRENDY2026_f09_S6 )
+           20260820_TRENDY2026_f09_S0 \
+           20260820_TRENDY2026_f09_S1 \
+           20260820_TRENDY2026_f09_S2 \
+           20260820_TRENDY2026_f09_S3 \
+           20260820_TRENDY2026_f09_S4 \
+           20260820_TRENDY2026_f09_S5 \
+           20260820_TRENDY2026_f09_S6 )
 
 
 OutDir=/scratch/hpcl-cli185/xyk/e3sm_runs/
